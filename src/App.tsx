1e983b8667046b09875a8236f494b5b4ab1b7ec5
Spencer Wilson <spencer.wilson@hey.com>
2020-08-26 02:19:13 -0400

    Add Instagram integration

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,27 @@ import NavDropdown from 'react-bootstrap/NavDropdown'
 import Form from 'react-bootstrap/Form'
 import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
-
+import Card from 'react-bootstrap/Card'
+import CardDeck from 'react-bootstrap/CardDeck'
+import InstagramEmbed from 'react-instagram-embed'
 
 function App() {
   return (
+  <>
   <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="">Home</Navbar.Brand>
+  <Navbar.Brand href="">Naked Curls</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
       <Nav.Link href="">Home</Nav.Link>
+      <Nav.Link href="">Blog</Nav.Link>
+      <Nav.Link href="">Wiki</Nav.Link>
+      <Nav.Link href="">Story</Nav.Link>
+      <Nav.Link href="">Contact</Nav.Link>
       <NavDropdown title="Products" id="basic-nav-dropdown">
         <NavDropdown.Item href="">Shampoo</NavDropdown.Item>
-        <NavDropdown.Divider />
         <NavDropdown.Item href="">Conditioner</NavDropdown.Item>
-        <NavDropdown.Divider />
         <NavDropdown.Item href="">Gel</NavDropdown.Item>
-        <NavDropdown.Divider />
       </NavDropdown>
     </Nav>
     <Form inline>
@@ -32,6 +36,57 @@ function App() {
     </Form>
   </Navbar.Collapse>
 </Navbar>
+<CardDeck>
+<Card>
+  <Card.Body>
+<InstagramEmbed
+  url='https://instagr.am/p/CATajz-F5YN/'
+  maxWidth={320}
+  hideCaption={false}
+  containerTagName='div'
+  protocol=''
+  injectScript
+  onLoading={() => {}}
+  onSuccess={() => {}}
+  onAfterRender={() => {}}
+  onFailure={() => {}}
+/>
+  </Card.Body>
+</Card>
+<Card>
+  <Card.Body>
+<InstagramEmbed
+  url='https://www.instagr.am/p/CEKgP76j8HA/'
+  maxWidth={320}
+  hideCaption={false}
+  containerTagName='div'
+  protocol=''
+  injectScript
+  onLoading={() => {}}
+  onSuccess={() => {}}
+  onAfterRender={() => {}}
+  onFailure={() => {}}
+/>
+  </Card.Body>
+</Card>
+<Card>
+  <Card.Body>
+<InstagramEmbed
+  url='https://www.instagram.com/p/CCguLi7piw7/'
+  maxWidth={320}
+  hideCaption={false}
+  containerTagName='div'
+  protocol=''
+  injectScript
+  onLoading={() => {}}
+  onSuccess={() => {}}
+  onAfterRender={() => {}}
+  onFailure={() => {}}
+/>
+  </Card.Body>
+</Card>
+</CardDeck>
+</>
   );
 }
 
